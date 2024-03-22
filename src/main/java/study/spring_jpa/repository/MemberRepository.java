72a515ffef733f1f0c901e0fa67d7c165234fc84
이정민 <kyungheeyj45@ajou.ac.kr>
2024-03-22 21:20:21 +0900

    섹션 3 : 공통 인터페이스 기능

--- src/main/java/study/spring_jpa/repository/MemberRepository.java
+++ src/main/java/study/spring_jpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package study.spring_jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.spring_jpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
 }
